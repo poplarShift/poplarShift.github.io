84d7fa9062ceadfbc02c9a743934117ab29c1c74
poplarShift <poplarShift@users.noreply.github.com>
2019-05-30 11:23:33 -0400

    util to clean bibfiles from unwanted fields

---
+++ _bibliography/clean_refs.py
@@ -0,0 +1,15 @@
+fname = 'webpage_refs.bib'
+
+omits = ['file =', 'owner =']
+
+with open(fname) as f:
+    lines = f.readlines()
+    cleaned_lines = [line for line in lines if not any(omit in line for omit in omits)]
+
+# probably not super smart to delete the bibfile in case of exceptions, but
+# then again it's only a bibfile
+import os
+os.remove(fname)
+
+with open(fname, 'w') as f:
+    f.writelines(cleaned_lines)
