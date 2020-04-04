eb7f8e3f41c50a0a0fe77f51d8a32bdd489a00c2
poplarShift <poplarShift@users.noreply.github.com>
2020-04-04 13:29:25 -0400

    add nitrate flux paper

--- _bibliography/clean_refs.py
+++ _bibliography/clean_refs.py
@@ -1,10 +1,13 @@
+#!/usr/bin/env python
+
 fname = 'webpage_refs.bib'
 
-omits = ['file =', 'owner =', 'shorttitle =', 'note =', 'bibtex[']
+omits = ['file =', 'owner =', 'abstract =', 'copyright', 'shorttitle =', 'note =', 'bibtex[']
 
 with open(fname) as f:
     lines = f.readlines()
     cleaned_lines = [line for line in lines if not any(omit in line for omit in omits)]
+
 
 # probably not super smart to delete the bibfile in case of exceptions, but
 # then again it's only a bibfile
